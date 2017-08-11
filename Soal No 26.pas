43c4d9b1d8a1261e9a103cda4a621a0b819f3d6c
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-11 20:03:24 +0700

    Update Soal No 26.pas

--- Soal No 26.pas
+++ Soal No 26.pas
@@ -5,15 +5,15 @@ begin
 if (x>1) then
 begin
 hasil:=hasil+1;
-writeln(x,'X');
+writeln(x:5,(x div 2):5,(x mod 2):6,((x div 2) + (x mod 2)):6,hasil:7);
 solve(X div 2 + X mod 2);
 
 end;
 end;
 begin
 N:=77;
-writeln('hasil':7,'x':7,'div':7,'mod':7,'div+mod':10);
+writeln('x':5,'div':6,'mod':6,'div+mod':8,'hasil':6);
 solve(N);
-writeln(hasil);
+writeln('Hasilnya adalah : ',hasil);
 readkey;
 end.
