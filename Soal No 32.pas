1077287ebc36031da232721683d74f3da6a2109b
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-11 20:07:34 +0700

    Update Soal No 32.pas

--- Soal No 32.pas
+++ Soal No 32.pas
@@ -1,13 +1,13 @@
-
+uses wincrt;
 procedure tulis(n,m:integer);
 var
-i,j,k:integer;
+i,j,k,l:integer;
 begin
 for i:=1 to n do
 begin
 for j:=1 to (n div m) do
 for k:=1 to m do
-writeln(i,'*');
+writeln('i = ':10,i:1,' *','j = ':10,j:1,' *','k = ':10,k:1,' *');
 for j:=1 to (n mod m) do
 writeln('-'); 
 end; 
@@ -15,9 +15,9 @@ end;
 var
 P,L: integer;
 begin
-readln(P);
-readln(L);
+writeln('I':10,'J':13,'K':13);
+P:=30;
+L:=30;
 tulis(P,L);
-readln;
+readkey;
 end.
-
