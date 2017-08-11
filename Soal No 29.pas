d10ffb24be9a3bc9f78c6f21176c4671b7aab1a9
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-11 20:05:52 +0700

    Update Soal No 29.pas

--- Soal No 29.pas
+++ Soal No 29.pas
@@ -21,22 +21,23 @@ begin
  Z[13]:=57;
  Z[14]:=14;
  Z[15]:=86;
+
+ writeln('X ',(x):1,'Y ':15,((x+y) div 2):1,'X ':15,((x+y) div 2+(1)):1,'Y ':15,(y):1);
  if (x = y) then
  f := Z[y]
 
  else begin
  a := f(x, (x+y) div 2);
  b := f((x+y) div 2+1, y);
- writeln('A : ',a);
- writeln('B : ',b);
+
+ writeln('  A : ':10,a:2,'B : ':30,b:2);
  if (a < b) then f := a
  else f := b
-
  end
-
 end;
 
 begin
+ writeln('(x)':2,'((x+y) div 2)':20,'((x+y) div 2+(1))':20,'(y)':8);
  we := f(3,11);
  writeln('Hasilnya adalah : ',we);
  readkey;
