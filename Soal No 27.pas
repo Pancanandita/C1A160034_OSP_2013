a5b39dbd6145e87a2abdbe2698bb53c71c1e3ff1
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-13 12:20:54 +0700

    Update Soal No 27.pas

--- Soal No 27.pas
+++ Soal No 27.pas
@@ -3,6 +3,7 @@ var
    kalimat,kata,base : String;
    i : integer;
 begin
+writeln('base = (QWERTYUIOPLKJHGFDSAZXCVBNM)');
 base := 'QWERTYUIOPLKJHGFDSAZXCVBNM';
 kata := '';
 kalimat :='s4yA-BuK4N+oRanG aLaY!?';
@@ -10,8 +11,9 @@ for i:= length(kalimat) downto 1 do
 begin
      if pos (kalimat[i], base) > 0 then
      kata:= kata + kalimat[i];
-     writeln(kata);
+     write('Kalimat ',kalimat,' Menyeleksi Kalimat dari base ',(kalimat[i]):2);
+     writeln('kata : ':10,kata);
 end;
-writeln('Hasilnya adalah : ',kata);
+writeln(‘Hasil kata adalah : ’,kata);
 readkey;
 end.
