84cb368a31d615aa94c36c5fa9157428e9a0a75d
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-13 12:22:47 +0700

    Update Soal No 43.pas

--- Soal No 43.pas
+++ Soal No 43.pas
@@ -1,15 +1,14 @@
 uses wincrt;
 var i,j,x: integer;
 begin
+writeln('i':5,'j':8,'(i+j)':12,'x := x + i + j':19);
  x := 0;
  for i:=1 to 5 do begin
  for j:= 5 downto 1 do begin
-
  x := x + i + j;
- writeln('i':5,'j':5,'x':5);
- writeln(i:5,j:5,x:5);
+ writeln(i:5,'+':4,j:4,'=':5,(i+j):5,x:8);
  end;
  end;
- writeln('Hasil : ',x);
+ writeln(‘Hasil : ’,x);
  readkey;
 end.
