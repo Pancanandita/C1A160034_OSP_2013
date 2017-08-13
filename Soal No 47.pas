0a8e6076882b7a8c34f57e82c79d46fd18f9dd38
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-13 10:14:05 +0700

    Update Soal No 47.pas

--- Soal No 47.pas
+++ Soal No 47.pas
@@ -3,28 +3,28 @@ uses wincrt;
 procedure f(x: longint; y: longint; z: longint);
 
  begin
+ //writeln((y):3,' (Y = 0) ':3,(y=0));
+ writeln;
  if (y = 0) then
- writeln(z)
+ writeln('Hasil Z adalah : ',z)
  else
- begin
+ begin 
+ writeln(x:3,y:5,z:5);
  if (y mod 2 = 1) then
  z := z + x;
  f(2*x, y div 2, z)
-
  end;
-  writeln(' x','  y ',' z');
-  writeln(x,' ',y,' ',z);
+ writeln(z:4,(z + x):7,(2*x):7,(y div 2):7);
  end;
 
  var
     a,b,c : longint;
  begin
+ writeln('x':3,'y':5,'z':5);
  a:=15;
  b:=97;
  c:=0;
- write('a : ');
- write('b : ');
- write('c = ');
  f(a,b,c);
+ writeln('z':4,'z+x':7,'2*x':7,'y div 2':10);
  readkey;
  end.
