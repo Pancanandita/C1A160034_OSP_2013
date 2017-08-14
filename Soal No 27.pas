8ab3504b8739d17a70bbe9ad7aa1b1abdf075138
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-14 18:40:42 +0700

    Update Soal No 27.pas
    Update Soal No 27.pas 
    Tgl 14 agustus 2017 / 18.40

--- Soal No 27.pas
+++ Soal No 27.pas
@@ -8,12 +8,17 @@ base := 'QWERTYUIOPLKJHGFDSAZXCVBNM';
 kata := '';
 kalimat :='s4yA-BuK4N+oRanG aLaY!?';
 for i:= length(kalimat) downto 1 do
+
 begin
+if pos (kalimat[i], base) > 0 then
+write(Kalimat[i],'in base':9,'YES':5);
+if pos (kalimat[i], base) <= 0 then
+write(Kalimat[i],'in base':9,'NO':5);
+
      if pos (kalimat[i], base) > 0 then
      kata:= kata + kalimat[i];
-     write('Kalimat ',kalimat,' Menyeleksi Kalimat dari base ',(kalimat[i]):2);
-     writeln('kata : ':10,kata);
+     writeln(' KATA = ':10,kata);
 end;
-writeln(‘Hasil kata adalah : ’,kata);
+writeln('Hasil kata adalah : ',kata);
 readkey;
 end.
