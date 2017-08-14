d508394a70b4dd9dbf509f9067653c340fa941bf
Akbar tawakal pancanandita basuki <30580298+Pancanandita@users.noreply.github.com>
2017-08-14 18:46:12 +0700

    Update Soal No 27.pas

--- Soal No 27.pas
+++ Soal No 27.pas
@@ -10,10 +10,10 @@ kalimat :='s4yA-BuK4N+oRanG aLaY!?';
 for i:= length(kalimat) downto 1 do
 
 begin
-if pos (kalimat[i], base) > 0 then
-write(Kalimat[i],'in base':9,'YES':5);
-if pos (kalimat[i], base) <= 0 then
-write(Kalimat[i],'in base':9,'NO':5);
+     if pos (kalimat[i], base) > 0 then
+     write(Kalimat[i],'in base':9,'YES':5);
+     if pos (kalimat[i], base) <= 0 then
+     write(Kalimat[i],'in base':9,'NO':5);
 
      if pos (kalimat[i], base) > 0 then
      kata:= kata + kalimat[i];
